449582cb63618e5ebbc4ab536c7e88f03b42627e
Bakr Frag <bakr.frag@gmail.com>
2023-10-30 10:49:22 +0200

    migrate user as an entity to db

--- app/db/dependancies.py
+++ app/db/dependancies.py
@@ -1,5 +1,8 @@
 from .database import SessionLocal , engine
+from app.models.user import User
 
+#migrate User Table to Database
+User.Base.metadata.create_all(bind=engine)
 # Dependency
 def get_db():
     db = SessionLocal()
