ddde93fc282006b5d8105282d5acf9135ce796f5
Bakr Frag <bakr.frag@gmail.com>
2023-10-30 13:01:11 +0200

    add room model

---
+++ app/models/room.py
@@ -0,0 +1,13 @@
+from sqlalchemy import  Column, Integer, String , Enum , Float
+from app.db.database import Base 
+
+
+
+class Room(Base):
+    __tablename__ = "room"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String ,unique=True )
+    price = Column(Float , nullable = False)
+    type = Column(Enum("Single","Double","Suit",name="room_types"),nullable = False)
+    
