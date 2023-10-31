46f78c78dd64419c97f9ac6ca8cc2937468b7089
Bakr Frag <bakr.frag@gmail.com>
2023-10-31 08:48:55 +0200

    implement api routers for reservation entity

--- app/db/dependancies.py
+++ app/db/dependancies.py
@@ -1,9 +1,11 @@
 from .database import SessionLocal , engine
 from app.models import user as user_model
 from app.models import room as room_model
+from app.models import reservation as reservation_model
 #migrate User Table to Database
 user_model.Base.metadata.create_all(bind=engine)
 room_model.Base.metadata.create_all(bind=engine)
+reservation_model.Base.metadata.create_all(bind=engine)
 # Dependency
 def get_db():
     db = SessionLocal()
---
+++ app/routers/reservation.py
@@ -0,0 +1,70 @@
+from fastapi import Depends, HTTPException , APIRouter
+from sqlalchemy.orm import Session
+from app.db.dependancies import get_db
+from app.schemas.reservation import InReservationModel , OutReservationModel
+from app.services import reservation
+from typing import List 
+
+router = APIRouter(
+    prefix="/reservation",
+    tags=["reservation"],
+    dependencies=[Depends(get_db)],
+    responses={404: {"description": "Not found"}},
+)
+
+@router.post("/add/",response_model=OutReservationModel)
+def add_reservation(reservation_data:InReservationModel , db:Session= Depends(get_db)) -> OutReservationModel:
+    """
+    add new reservation object in database 
+    """
+    
+    reservation_object = reservation.add_reservation(db , reservation_data)
+    return reservation_object
+
+
+@router.get("/all/",response_model = List[OutReservationModel])
+def get_all_reservations(db:Session = Depends(get_db)) -> List[OutReservationModel]:
+    """
+    get all reservations
+    """
+    return reservation.get_all_reservations(db)
+
+
+# @router.put("/{reservation_id}/", response_model = OutReservationModel)
+# def update_reservation(reservation_id:int , reservation_data:InReservationModel , db:Session = Depends(get_db)):
+#     """
+#     update reservation data
+#     """
+#     reservation_by_id = reservation.get_reservation_by_id(db , reservation_id)
+#     if not reservation_by_id:
+#         raise HTTPException(status_code= 400 , detail = f"no reservation with id {reservation_id}")
+    
+#     reservation_code = reservation_data.code.lower()
+#     if reservation.get_reservation_by_code(db , reservation_code):
+#         raise HTTPException(status_code= 400 , detail = f"reservation with code {reservation_code} exists")
+
+#     return reservation.update_reservation(db, reservation_id , reservation_data)
+
+@router.delete("/{reservation_id}/")
+def delete_reservation(reservation_id:int , db:Session = Depends(get_db)):
+    """
+    delete reservation
+    """
+    reservation_by_id = reservation.get_reservation_by_id(db , reservation_id)
+    if not reservation_by_id:
+        raise HTTPException(status_code= 400 , detail = f"no reservation with id {reservation_id}")
+
+    reservation.delete_reservation(db , reservation_id)
+    return {
+        "message":"reservation removed successfully"
+    }
+
+@router.get("/{reservation_id}/", response_model= OutReservationModel)
+def get_reservation_by_id(reservation_id : int , db:Session = Depends(get_db)):
+    """
+    get full reservation detail by id 
+    """
+    reservation_by_id = reservation.get_reservation_by_id(db , reservation_id)
+    if not reservation_by_id:
+        raise HTTPException(status_code= 400 , detail = "no reservation with parsed id")
+    return reservation_by_id
